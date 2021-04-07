b5278031d84ca9185eab89992452dbd7d7de9978
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-04-07 22:18:34 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function mixUp() {
+	var input = document.getElementById("input").value.split("\n");
+	var shuffled = shuffle(input);
+
+	var output = document.getElementById("output");
+shuffled.forEach(function(currentNumber) {
+    output.innerHTML += currentNumber + "\n";
+})
+}
+///Stack Overflow 
